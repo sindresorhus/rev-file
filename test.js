ee13ebe4510d365e8b830b92d486c88405c07cae
Sindre Sorhus <sindresorhus@gmail.com>
2017-01-07 14:33:43 +0700

    Make XO happy

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 import test from 'ava';
-import fn from './';
+import m from './';
 
-const reRevved = /test\-[\d\w]{10}\.js/;
+const reRevved = /test-[\d\w]{10}\.js/;
 
 test(async t => {
-	t.true(reRevved.test(fn.sync('test.js')));
-	t.true(reRevved.test(await fn('test.js')));
+	t.true(reRevved.test(m.sync('test.js')));
+	t.true(reRevved.test(await m('test.js')));
 });
